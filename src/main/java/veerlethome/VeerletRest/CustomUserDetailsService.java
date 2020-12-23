c841774eb0e9ce1f07d67e5856dc06b139158c1d
unknown <payusakpovero@gmail.com>
2020-12-23 21:46:01 +0100

    make

--- src/main/java/veerlethome/VeerletRest/CustomUserDetailsService.java
+++ src/main/java/veerlethome/VeerletRest/CustomUserDetailsService.java
@@ -1,10 +1,13 @@
 package veerlethome.VeerletRest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -17,5 +20,9 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
       VeerletHomeModel user=repository.findByUserName(username);
       return new org.springframework.security.core.userdetails.User(user.getUsername(),user.getPassword(), new ArrayList<>());
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
