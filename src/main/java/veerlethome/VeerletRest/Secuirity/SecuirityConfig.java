397512e71610a7971079459089ecb86d8041c18f
unknown <payusakpovero@gmail.com>
2020-12-25 20:11:45 +0100

    akpos

--- src/main/java/veerlethome/VeerletRest/Secuirity/SecuirityConfig.java
+++ src/main/java/veerlethome/VeerletRest/Secuirity/SecuirityConfig.java
@@ -2,15 +2,19 @@ package veerlethome.VeerletRest.Secuirity;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import veerlethome.VeerletRest.Services.CustomUserDetailsService;
 
+@EnableWebSecurity
+@Configuration
 public class SecuirityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
    private CustomUserDetailsService customUserDetailsService;
@@ -31,8 +35,9 @@ public class SecuirityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Bean
     public AuthenticationManager authenticationManagerBean()throws Exception{
         return super.authenticationManagerBean();
     }
+
 }
