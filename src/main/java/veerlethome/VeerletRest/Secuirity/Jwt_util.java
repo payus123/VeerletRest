6a9a3d9a6dcb822c1ddbcb28aa4c7e830cdf89be
unknown <payusakpovero@gmail.com>
2021-01-02 17:26:58 +0100

    make it work

--- src/main/java/veerlethome/VeerletRest/Secuirity/Jwt_util.java
+++ src/main/java/veerlethome/VeerletRest/Secuirity/Jwt_util.java
@@ -3,6 +3,7 @@ package veerlethome.VeerletRest.Secuirity;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +14,8 @@ import java.util.function.Function;
 
 @Service
 public class Jwt_util {
-
-    private String SECRET_KEY = "secret";
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
